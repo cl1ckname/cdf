007453be98af659acb8dcbdc9f0e29f523cd35c1
Ivan Vasilev <20002mc@gmail.com>
2025-01-13 22:51:27 +0300

    add tests

---
+++ internal/utils/func.go
@@ -0,0 +1,23 @@
+package utils
+
+func PtrEq[T comparable](a, b *T) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a != nil && b != nil {
+		return *a == *b
+	}
+	return false
+}
+
+func ArrayEq[T comparable](a, b []T) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
